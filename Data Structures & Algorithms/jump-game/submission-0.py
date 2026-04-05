d4b12d23c432e4e590f3c0816fbd9bc2a1f26479
Pratham Sitoula <63824868+pratham124@users.noreply.github.com>
2026-04-05 16:31:37 -0600

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        q = deque([0])
+
+
+        target = len(nums) - 1
+        visit = set()
+
+        while q:
+            for _ in range(len(q)):
+                cur = q.popleft()
+                visit.add(cur)
+                if cur == target:
+                    return True
+                
+                for j in range(1, nums[cur] + 1):
+                    if (cur + j) in visit:
+                        continue
+                    q.append(cur + j)
+        return False
